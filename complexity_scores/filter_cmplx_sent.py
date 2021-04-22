a9b2d5614e54deb4d14ad8c395698f21d66c844d
rumesh <rumesh.m.s16@gmail.com>
2021-04-22 11:54:47 +0530

    added filtering by SPW

--- complexity_scores/filter_cmplx_sent.py
+++ complexity_scores/filter_cmplx_sent.py
@@ -1,4 +1,7 @@
-from cal_scores import *
+import datetime
+import json
+
+from cal_scores import words_in_sentence, dataset_file_path, is_sinhala_word, word_length
 
 
 def filter_long_sentences(sent_list, threshold):
@@ -10,9 +13,22 @@ def filter_long_sentences(sent_list, threshold):
     return result
 
 
+def filter_by_syllables_per_word(sent_list, word_len_thresh, freq_thresh):
+    result = []
+    for sent in sent_list:
+        words = sent.split(" ")
+        freq = 0
+        for word in words:
+            if is_sinhala_word(word) and word_length(word) >= word_len_thresh:
+                freq += 1
+        if freq >= freq_thresh:
+            result.append(sent)
+    return result
+
+
 def create_output_file(output_data, ending="", extension="txt"):
-    s = str(uuid.uuid4()) + "_" + ending + "." + extension
-    with open(s, "w") as f:
+    s = datetime.datetime.now().strftime("%d-%B-%Y-%I-%M-%S-%s-%p") + "_" + ending + "." + extension
+    with open(s, "w", encoding='utf8') as f:
         if type(output_data) == list:
             string_data = ""
             for line in output_data:
@@ -20,14 +36,19 @@ def create_output_file(output_data, ending="", extension="txt"):
             f.write(string_data)
         elif type(output_data) == str:
             f.write(output_data)
+        elif type(output_data) == dict:
+            json.dump(output_data, f, ensure_ascii=False)
         else:
-            raise TypeError("Un supported format")
+            raise TypeError("Unsupported input type")
 
 
 if __name__ == '__main__':
     file_data = open(dataset_file_path)
     data = file_data.readlines()
-    filtered = filter_long_sentences(data, 70)
-    print("Number od sentences: ", len(filtered))
-    print(filtered)
-    create_output_file(filtered, "", "txt")
+    filtered_long = filter_long_sentences(data, 70)
+    print("Number of long sentences: ", len(filtered_long))
+    create_output_file(filtered_long, "Len", "txt")
+
+    filtered_spw_sent = filter_by_syllables_per_word(filtered_long, 7, 5)
+    print("Filtered by SPW: ", len(filtered_spw_sent))
+    create_output_file(filtered_spw_sent, "SPW", "txt")
