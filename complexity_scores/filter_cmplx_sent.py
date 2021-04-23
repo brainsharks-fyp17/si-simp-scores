76a1dac1c28406b002dbe76902de6149ab985db9
rumesh <rumesh.m.s16@gmail.com>
2021-04-23 10:43:34 +0530

    filtering cluttered sentences

--- complexity_scores/filter_cmplx_sent.py
+++ complexity_scores/filter_cmplx_sent.py
@@ -3,12 +3,19 @@ import json
 
 from cal_scores import words_in_sentence, dataset_file_path, is_sinhala_word, word_length
 
+MAX_NO_PHRASES_IN_SENT = 1
+
+
+# cluttered sentences contain phrases connected by commas
+def is_cluttered_sent(sent):
+    return sent.count(",") >= MAX_NO_PHRASES_IN_SENT or sent.count(" . ") >= MAX_NO_PHRASES_IN_SENT
+
 
 def filter_long_sentences(sent_list, threshold):
     result = []
     for sent in sent_list:
         sent = sent.strip().replace('\u200d', '')
-        if words_in_sentence(sent) > threshold:
+        if words_in_sentence(sent) > threshold and not is_cluttered_sent(sent):
             result.append(sent)
     return result
 
@@ -43,12 +50,17 @@ def create_output_file(output_data, ending="", extension="txt"):
 
 
 if __name__ == '__main__':
+    SENTENCE_LEN_THRESHOLD = 25
+    SPW_THRESH = 5
+    SPW_IN_SENT_THRESH = 7
+
     file_data = open(dataset_file_path)
     data = file_data.readlines()
-    filtered_long = filter_long_sentences(data, 70)
+    print("Number of all sentences: ", len(data))
+    filtered_long = filter_long_sentences(data, SENTENCE_LEN_THRESHOLD)
     print("Number of long sentences: ", len(filtered_long))
     create_output_file(filtered_long, "Len", "txt")
 
-    filtered_spw_sent = filter_by_syllables_per_word(filtered_long, 7, 5)
+    filtered_spw_sent = filter_by_syllables_per_word(filtered_long, SPW_THRESH, SPW_IN_SENT_THRESH)
     print("Filtered by SPW: ", len(filtered_spw_sent))
     create_output_file(filtered_spw_sent, "SPW", "txt")
