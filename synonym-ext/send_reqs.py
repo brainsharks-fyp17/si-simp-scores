21a2e1e9d9cedf67908e65c0a9acb4b800598723
rumesh <rumesh.m.s16@gmail.com>
2021-05-22 07:39:36 +0530

    insert to DB

---
+++ synonym-ext/send_reqs.py
@@ -0,0 +1,22 @@
+import json
+
+import grequests
+
+url = 'http://localhost:3000/insert'
+file_name = 'si_similar_beauti.json'
+resultList = []
+f = open(file_name)
+all_dict = json.load(f)
+dict_keys_list = list(all_dict.keys())
+for i in range(1000):
+    comp = dict_keys_list[i]
+    values = ""
+    for j in all_dict[comp]:
+        values += j + ","
+    values = values[:len(values) - 1]
+    jsn = dict()
+    jsn["c"] = comp
+    jsn["v"] = values
+    action_item = grequests.post(url, json=jsn)
+    resultList.append(action_item)
+print(grequests.map(resultList))
