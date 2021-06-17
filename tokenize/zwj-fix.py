2feba4dab6a9a4a1e05e843001dd19cb4ee4fe2d
rumesh <rumesh.m.s16@gmail.com>
2021-06-17 14:54:48 +0530

    zwj fix ,clean

--- tokenize/zwj-fix.py
+++ tokenize/zwj-fix.py
@@ -1,9 +1,24 @@
+import re
 from typing import Text
 
+# unicode values of chars
 zwj = 8205  # \u200d
 halkirima = 3530  # '්'
 rayanna = 3515  # 'ර',
 yayanna = 3514  # 'ය'
+
+sinhala_start = 3456
+vowels_and_const_end = 3527
+sinhala_end = 3573
+
+
+def is_sinhala_letter_or_allowed_sym(letter):
+    if letter == " " or letter == "#":  # or letter == "$":
+        return True
+    unicode_val = ord(letter)
+    # only Sinhala vowels and constants
+    if sinhala_start <= unicode_val <= sinhala_end:
+        return True
 
 
 def fix_zwj(text: Text) -> Text:
@@ -25,20 +40,42 @@ def fix_zwj(text: Text) -> Text:
     return "".join(new_chr_list)
 
 
+def clean(text: Text) -> Text:
+    """
+    Remove all non Sinhala characters.
+    Replace numbers with '#' and English words with '$'
+    :param text: Sinhala sentence
+    :return: cleaned Sinhala sentence
+    """
+    text = text.strip()
+    text = re.sub(r"[0-9]+", "#", text)
+    # text = re.sub(r"\b[A-Za-z]+", "$", text)
+
+    cleaned_sent = ''
+    for char in text:
+        if is_sinhala_letter_or_allowed_sym(char):
+            cleaned_sent += char
+    tokens = cleaned_sent.split()  # split on whitespaces
+    sent = ' '.join(tokens)
+    return sent
+
+
 if __name__ == '__main__':
-    sentences = ["ඉන්දුනීසියාවේ සුමාත්‍රා දිවයිනේ මුසී ගංඟාවේ මගී යාත්‍රාවක් අනතුරට ලක්වීමෙන් පුද්ගලයන් 13 දෙනෙකු "
-                 "ජීවිතක්ෂයට පත්වුණා",
+    sentences = ["ඉන්දුනීසියාවේ සුමාත්‍රා දිවයිනේ මුසී ගංඟාවේ මගී යාත්‍රාවක් අනතුරට ලක්වීමෙන් පුද්ගලයන් 13 දෙනෙකු ",
+                 "ජීවිතක්ෂයට පත්වුණා Hello , 6 39 // * chi sk s erefකුසලානෙට සි‍ලෝන්vfrv  බාබේරfvcfියන්",
                  "බ්‍රැඩ්බි කුසලානෙට ට්‍රිනිට් එකත් එක්ක සෙල්ලම් කරපු එකයි සි‍ලෝන් බාබේරියන්",
+                 "බ්රැඩ්බි කුසලානෙට ට්රිනිට් එකත් එක්ක ",
                  "බෞද්ධ කටයුතු දෙපාර්තමේන්තුව",
+                 "ශාස්ත්රීය භාවිතයක් විවේචනය ",
                  "ශාස්ත්‍රීය භාවිතයක් විවේචනය ",
-                 "මාධ්‍ය සදාචාරය යහපත්",
+                 "මාධ්‍ය සදාචාරය යහපත් ක්රියාකාරිත්වය",
                  "එවැනි තක්සේරුවක් තමයි ඔවුන් පේ‍්‍රක්ෂකයින්ට ලබා දීලා තියෙන්නෙ",
                  "අධි වේගී අධි ධාරිතා ‍ෆ්‍රී වයි ෆයි සේවය ලබා දුන්න",
-                 "ප්‍රසාරණය වීම සඳහා මේ ව්‍යාපාරවලට ප්‍රාග්ධනය අවශ්‍යයි"
+                 "ප්‍රසාරණය වීම සඳහා මේ ව්‍යාපාරවලට ප්‍රාග්ධනය අවශ්‍යයි ",
                  "දරුණු ප‍්‍රහාරයක් එල්ල කළද ඒවා ව්‍යර්ථ",
                  "අග්‍රාමාත්‍ය රනිල් වික්‍ර‍මසිංහ"
                  ]
-    r = ""
+    r = []
     for i in sentences:
-        r += fix_zwj(i) + "\n"
-    open("out-zwj.txt", "w").write(r)
+        r.append((clean(i)))
+    open("out-zwj.txt", "w").write('\n'.join(r))
