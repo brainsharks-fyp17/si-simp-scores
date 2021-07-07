6d10d006145a963ea9d10047a079a14b82e4923a
rumesh <rumesh.m.s16@gmail.com>
2021-07-07 15:41:50 +0530

    forward fixed

--- torch-ex/lm.py
+++ torch-ex/lm.py
@@ -1,6 +1,7 @@
+import linecache
 import os
 from argparse import Namespace
-from typing import List, Text, Tuple
+from typing import List, Text
 
 import nltk
 import numpy as np
@@ -8,13 +9,37 @@ import pandas as pd
 import sentencepiece as spm
 import torch
 import torch.nn as nn
-
 from torch import optim
 from torch.utils.data import Dataset, DataLoader
-from torch.utils.data.dataset import T_co
+import torch.nn.functional as F
 from tqdm import tqdm
 from tqdm import tqdm_notebook
-import linecache
+
+args = Namespace(
+    # Data and Path information
+    model_state_file="model.pth",
+    vocab_file="/home/rumesh/Downloads/FYP/sentencepiece-vocabs/m-30000-bpe.model",
+    data_file="/home/rumesh/Downloads/FYP/datasets/15m-tokenized/tokenized_shard_100000.txt",
+    train_data_file="cbow_out_train.tsv",
+    validation_data_file="cbow_out_validation.tsv",
+    test_data_file="cbow_out_test.tsv",
+    save_dir="model_storage/cbow-si",
+    sentence_length=12,
+    # Model hyper parameters
+    embedding_size=50,
+    hidden_size=300,
+    # Training hyper parameters
+    seed=1337,
+    num_epochs=10,
+    learning_rate=0.0001,
+    batch_size=32,
+    early_stopping_criteria=5,
+    # Runtime options
+    cuda=False,
+    catch_keyboard_interrupt=True,
+    reload_from_files=False,
+    expand_filepaths_to_save_dir=True
+)
 
 
 class LanguageModel(nn.Module):
@@ -34,26 +59,29 @@ class LanguageModel(nn.Module):
 
         self.fc = nn.Linear(hidden_size, self.output_size)
 
-    def forward(self, input_seq, tgt_seq):
-        input_seq = self.dropout(self.embedding(input_seq))
-        out, hidden = self.rnn(input_seq, None)
+    def forward(self, x_in, apply_softmax=False):
+        embed_out = self.embedding(x_in)
+        embed_out = self.dropout(embed_out)
+        out, hidden = self.rnn(embed_out, None)
 
-        out = self.fc(self.dropout(out))
-        loss_func = nn.CrossEntropyLoss(ignore_index=self.pad_idx)
+        y_out = self.fc(self.dropout(out))
+        # loss_func = nn.CrossEntropyLoss(ignore_index=self.pad_idx)
 
-        out = out.view(-1, out.size(-1))
-        tgt_seq = tgt_seq.view(-1)
+        # out = out.view(-1, out.size(-1))
+        # tgt_seq = tgt_seq.view(-1)
+        #
+        # loss = loss_func(out, tgt_seq)
+        if apply_softmax:
+            y_out = F.softmax(y_out, dim=1)
 
-        loss = loss_func(out, tgt_seq)
-
-        return loss
+        return y_out
 
 
 class Vocabulary:
-    def __init__(self, file_name: Text, mask_index=0):
+    def __init__(self, file_name: Text, pad_index=0):
         self.sp = spm.SentencePieceProcessor()
         self.sp.load(file_name)
-        self.mask_index = mask_index
+        self.pad_index = pad_index
 
     def get_ids(self, sentence: Text) -> List[int]:
         return self.sp.encode_as_ids(sentence)
@@ -80,8 +108,8 @@ class Vectorizer:
         x = np.zeros(sentence_length, dtype=np.int64)
         x[:len(ids)] = ids
 
-        if self.vocab.mask_index != 0:
-            x[len(ids):] = self.vocab.mask_index
+        if self.vocab.pad_index != 0:
+            x[len(ids):] = self.vocab.pad_index
 
         return x
 
@@ -92,8 +120,8 @@ class Vectorizer:
         x = np.zeros(sentence_length, dtype=np.int64)
         x[:len(lst)] = lst
 
-        if self.vocab.mask_index != 0:
-            x[len(lst):] = self.vocab.mask_index
+        if self.vocab.pad_index != 0:
+            x[len(lst):] = self.vocab.pad_index
 
         return x
 
@@ -108,12 +136,11 @@ class CBOWDataset(Dataset):
         return {'x_data': self.vectorizer.fill_with_mask(lst, self.sentence_length),
                 'y_target': target}
 
-    def __init__(self, vectorizer: Vectorizer, data_file, batch_size, sentence_length):
+    def __init__(self, vectorizer: Vectorizer, data_file, sentence_length):
         self.vectorizer = vectorizer
         self.train_ids = []
         self.test_ids = []
         self.validation_ids = []
-        self.batch_size = batch_size
         self.current_idx = 0
         self.data_file = data_file
         self.sentence_length = sentence_length
@@ -135,15 +162,13 @@ class CBOWDataset(Dataset):
     @staticmethod
     def create_cbow_csv(vectorizer: Vectorizer,
                         data_file: Text,
-                        out_file_name="cbow_out",
                         train_proportion=0.7,
                         val_proportion=0.15,
                         window_size=5) -> None:
         """
         :param vectorizer: Vectorizer
         :param data_file: path of the file that contains sentences. One line should contain only one sentence.
-        Data should have been cleaned.
-        :param out_file_name: Path of the output tsv
+        Data is assumed been cleaned.
         :param train_proportion: train proportion
         :param val_proportion: validation proportion
         :param window_size: window size
@@ -185,15 +210,15 @@ class CBOWDataset(Dataset):
 
         cbow_data['split'] = cbow_data.apply(lambda row: get_split(row.name), axis=1)
         cbow_data_train = cbow_data.loc[cbow_data['split'] == 'tr']
-        cbow_data_train.to_csv(out_file_name + "_train.tsv", index=False, sep='\t', index_label=False, header=False,
+        cbow_data_train.to_csv(args.train_data_file, index=False, sep='\t', index_label=False, header=False,
                                mode="w")
 
         cbow_data_test = cbow_data.loc[cbow_data['split'] == 'te']
-        cbow_data_test.to_csv(out_file_name + "_test.tsv", index=False, sep='\t', index_label=False, header=False,
+        cbow_data_test.to_csv(args.test_data_file, index=False, sep='\t', index_label=False, header=False,
                               mode="w")
 
         cbow_data_validation = cbow_data.loc[cbow_data['split'] == 'va']
-        cbow_data_validation.to_csv(out_file_name + "_validation.tsv", index=False, sep='\t', index_label=False,
+        cbow_data_validation.to_csv(args.validation_data_file, index=False, sep='\t', index_label=False,
                                     header=False,
                                     mode="w")
 
@@ -201,26 +226,10 @@ class CBOWDataset(Dataset):
         return len(self.test_ids) + len(self.train_ids) + len(self.validation_ids)
 
     def num_batches(self) -> int:
-        return len(self) // self.batch_size
+        return len(self) // args.batch_size
 
-    # def train_batch_generator(self, input_len=10, target_len=2):
-    #     val_start_id = self.validation_ids[0]
-    #     out = []
-    #     for i, line in enumerate(self.file):
-    #         if val_start_id == i:
-    #             break
-    #         items = line.strip().split("\t")
-    #         sent = items[0]
-    #         target = items[1]
-    #         x = self.vectorizer.vectorize(sent, input_len)
-    #         y = self.vectorizer.vectorize(target, target_len)
-    #         if not (x is None or y is None):
-    #             out.append((x, y))
-    #         if len(out) == self.batch_size:
-    #             temp = out[:]  # copy
-    #             out = []
-    #             yield temp
-    def set_split(self, param):
+    @staticmethod
+    def set_split(param):
         print("Loaded dataset", param)
 
 
@@ -235,25 +244,6 @@ def handle_dirs(dirpath):
     if not os.path.exists(dirpath):
         os.makedirs(dirpath)
 
-
-args = Namespace(
-    # Data and Path information
-    model_state_file="model.pth",
-    save_dir="model_storage/cbow-si",
-    # Model hyper parameters
-    embedding_size=50,
-    # Training hyper parameters
-    seed=1337,
-    num_epochs=10,
-    learning_rate=0.0001,
-    batch_size=32,
-    early_stopping_criteria=5,
-    # Runtime options
-    cuda=False,
-    catch_keyboard_interrupt=True,
-    reload_from_files=False,
-    expand_filepaths_to_save_dir=True
-)
 
 if args.expand_filepaths_to_save_dir:
     # args.vectorizer_file = os.path.join(args.save_dir,
@@ -362,12 +352,13 @@ def generate_batches(dataset: Dataset, batch_size: int, shuffle=True,
 
 
 def train():
-    vocab = Vocabulary("/home/rumesh/Downloads/FYP/sentencepiece-vocabs/m-30000-bpe.model")
+    vocab = Vocabulary(args.vocab_file)
     vec = Vectorizer(vocab=vocab)
-    dataset_train = CBOWDataset(vectorizer=vec, data_file='cbow_out_train.tsv', batch_size=32, sentence_length=12)
+    dataset_train = CBOWDataset(vectorizer=vec, data_file=args.train_data_file,
+                                sentence_length=args.sentence_length)
     # dataset_test = CBOWDataset(vectorizer=vec, data_file='cbow_out_test.tsv', batch_size=32, sentence_length=12)
-    dataset_validation = CBOWDataset(vectorizer=vec, data_file='cbow_out_validation.tsv', batch_size=32,
-                                     sentence_length=12)
+    dataset_validation = CBOWDataset(vectorizer=vec, data_file=args.validation_data_file,
+                                     sentence_length=args.sentence_length)
     lang_model = LanguageModel(emb_size=args.embedding_size, hidden_size=args.hidden_size, output_size=args.batch_size,
                                dropout_p=0.1, pad_idx=0)
     lang_model = lang_model.to(args.device)
@@ -378,9 +369,9 @@ def train():
                                                      mode='min', factor=0.5,
                                                      patience=1)
     train_state = make_train_state(args)
-    epoch_bar = tqdm_notebook(desc='training routine',
-                              total=args.num_epochs,
-                              position=0)
+    epoch_bar = tqdm(desc='training routine',
+                     total=args.num_epochs,
+                     position=0)
     try:
         for epoch_index in range(args.num_epochs):
             train_state['epoch_index'] = epoch_index
@@ -390,15 +381,15 @@ def train():
             # setup: batch generator, set loss and acc to 0, set train mode on
 
             dataset_train.set_split('train')
-            train_bar = tqdm_notebook(desc='split=train',
-                                      total=dataset_train.num_batches(),
-                                      position=1,
-                                      leave=True)
+            train_bar = tqdm(desc='split=train',
+                             total=dataset_train.num_batches(),
+                             position=1,
+                             leave=True)
             dataset_validation.set_split('val')
-            val_bar = tqdm_notebook(desc='split=val',
-                                    total=dataset_validation.num_batches(),
-                                    position=1,
-                                    leave=True)
+            val_bar = tqdm(desc='split=val',
+                           total=dataset_validation.num_batches(),
+                           position=1,
+                           leave=True)
             batch_generator_train = generate_batches(dataset_train,
                                                      batch_size=args.batch_size,
                                                      device="cuda" if args.cuda else "cpu")
@@ -445,14 +436,14 @@ def train():
 
             # setup: batch generator, set loss and acc to 0; set eval mode on
             dataset_validation.set_split('val')
-            batch_generator = generate_batches(dataset_validation,
-                                               batch_size=args.batch_size,
-                                               device="cuda" if args.cuda else "cpu")
+            batch_generator_validation = generate_batches(dataset_validation,
+                                                          batch_size=args.batch_size,
+                                                          device="cuda" if args.cuda else "cpu")
             running_loss = 0.
             running_acc = 0.
             lang_model.eval()
 
-            for batch_index, batch_dict in enumerate(batch_generator):
+            for batch_index, batch_dict in enumerate(batch_generator_validation):
                 # compute the output
                 y_pred = lang_model(x_in=batch_dict['x_data'])
 
@@ -488,35 +479,19 @@ def train():
 
 
 def create_split_datasets():
-    vo = Vocabulary("/home/rumesh/Downloads/FYP/sentencepiece-vocabs/m-30000-bpe.model")
+    vo = Vocabulary(args.vocab_file)
     vec = Vectorizer(vocab=vo)
     CBOWDataset.create_cbow_csv(vectorizer=vec,
-                                data_file="/home/rumesh/Downloads/FYP/datasets/15m-tokenized/tokenized_shard_100000.txt",
+                                data_file=args.data_file,
                                 )
 
 
 if __name__ == '__main__':
-    # vo = Vocabulary("/home/rumesh/Downloads/FYP/sentencepiece-vocabs/m-30000-bpe.model")
-    # sen = "<s>ඩෙංගු රෝගීන්ගෙන් ආසන්න වශයෙන් 423 ක් පමණ වාර්තා වන්නේ බස්නාහිර</s>"
-    # print(vo._get_pieces(sen))
-    # print(vo.get_ids(sen))
+    # vo = Vocabulary(args.vocab_file)
     # vec = Vectorizer(vocab=vo)
-    # x, y = vec.vectorize(sen)
-    # print(len(x), x)
-    # print(len(y), y)
-    # # print(vo.get_ids(sen))
-    # print(vo.get_sentence(x.tolist()))
-    # print(vo.get_sentence(y.tolist()))
-    # ids = [29925, 5, 4986, 4855, 491, 1948, 749, 7186, 29983, 121, 407, 770, 705, 4113, 0,0,0,0,0]
-    # print(vo.get_sentence(ids))
-    # CBOWDataset.create_cbow_csv(vectorizer=vec,
-    #                             data_file="/home/rumesh/Downloads/FYP/datasets/15m-tokenized/tokenized_shard_100000.txt",
-    #                             )
-    # data_set = CBOWDataset(vectorizer=vec, data_file='cbow_out.tsv', batch_size=32, sentence_length=12)
-    # print(dataset.__getitem__(0))
-    # print(data_set.__getitem__(1))
+    # data_set = CBOWDataset(vectorizer=vec, data_file=args.test_data_file, sentence_length=args.sentence_length)
     # batch_gen = generate_batches(dataset=data_set, batch_size=32)
-    # print(next(batch_gen))
-    # gen = dataset.train_batch_generator(input_len=10, target_len=2)
-    # train()
-    create_split_datasets()
+    # b1 = next(batch_gen)
+    # var = b1['x_data'].shape
+    # print(var)
+    train()
