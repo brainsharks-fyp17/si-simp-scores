e5b7e81d38ccffe6f1e08a38223930e28c8451cb
rumesh <rumesh.m.s16@gmail.com>
2021-09-13 09:51:47 +0530

    pivoting for paraphrases

--- synonym-ext/pivoting.py
+++ synonym-ext/pivoting.py
@@ -1,4 +1,5 @@
-from mtranslate import translate
+import multiprocessing
+import uuid
 
 english = "en"
 sinhala = "si"
@@ -11,26 +12,38 @@ turkish = "tr"
 thai = "th"
 pivot_lang = [german, chinese, arabic, french, russian, turkish, thai]
 
-
 f = open("all_words.txt").readlines()
 source = [line.strip() for line in f]
-file = open("ppdb-si-pivot.txt", "w")
-j = 0
-for word in source:
-    j += 1
-    try:
-        paraphrases = set()
-        for p in pivot_lang:
-            pivot = translate(word, to_language=p, from_language=sinhala)
-            out = translate(pivot, to_language=sinhala, from_language=p)
-            paraphrases.add(out)
-        print(j)
-        if len(paraphrases) == 1 and list(paraphrases)[0] == word:
-            continue
-        file.write(word + ": ")
-        for i in paraphrases:
-            file.write(i + ",")
-        file.write("\n")
-        file.flush()
-    except:
-        pass
+
+
+def do_pivoting(start, end):
+    uid = str(uuid.uuid4())
+    file = open("ppdb-si-pivot-" + uid + ".txt", "w")
+    from mtranslate import translate
+    print("started", start, end)
+    for k in range(start, end):
+        try:
+            paraphrases = set()
+            for p in pivot_lang:
+                pivot = translate(source[k], to_language=p, from_language=sinhala)
+                out = translate(pivot, to_language=sinhala, from_language=p)
+                paraphrases.add(out)
+            if len(paraphrases) == 1 and list(paraphrases)[0] == source[k]:
+                continue
+            file.write(source[k] + ": ")
+            for i in paraphrases:
+                file.write(i + ",")
+            file.write("\n")
+            file.flush()
+        except:
+            pass
+    print("ended", start, end)
+    file.close()
+
+
+n_processes = 5
+N = 10
+pool = multiprocessing.Pool(n_processes)
+chunks = [(i, i + N) for i in range(0, len(source), N)]
+print(chunks)
+pool.starmap(do_pivoting, chunks)
